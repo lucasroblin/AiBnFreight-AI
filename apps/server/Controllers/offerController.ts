f7fde80a6943dadaff49974a0a15214424f9a947
Arol Viñolas <arol@arol.dev>
2024-11-19 12:09:02 +0100

    feat(server): add pagination on /offers

--- apps/server/Controllers/offerController.ts
+++ apps/server/Controllers/offerController.ts
@@ -1,76 +1,100 @@
-import { Request, Response } from "express";
-import { _addBooking, _addOffer, _getAllOffers, _getBooking, _getOffer} from "../Models/offerModel";
-import { Booking, Offer } from "@prisma/client";
+import { Request, Response } from 'express'
+import * as OfferRepository from '../Models/offerModel'
+import { Booking, Offer } from '@prisma/client'
 
-export async function addOffer(req: Request,res: Response): Promise<void>{
-    const {departure, arrival, weight, etd, eta, price, notes} = req.body
-    await _addOffer(departure, arrival, weight, etd, eta, price, notes)
-    res.status(201).json({statusCode : 201, message : "Offer added"})
+export async function addOffer(req: Request, res: Response): Promise<void> {
+  const { departure, arrival, weight, etd, eta, price, notes } = req.body
+  await OfferRepository.addOffer(
+    departure,
+    arrival,
+    weight,
+    etd,
+    eta,
+    price,
+    notes
+  )
+  res.status(201).json({ statusCode: 201, message: 'Offer added' })
 }
 
-export async function getOffer(req: Request, res: Response): Promise<void>{
-    const {id} = req.params
-    const offer = await _getOffer(Number(id))
-    if (!!offer){
-        res.status(201)
-        res.json({
-            statusCode : 201,
-            data : offer
-        })
-    } else{
-        res.status(404)
-        res.json("Offer not found")
-    }
+export async function getOffer(req: Request, res: Response): Promise<void> {
+  const { id } = req.params
+  const offer = await OfferRepository.getOffer(Number(id))
+  if (!!offer) {
+    res.status(201)
+    res.json({
+      statusCode: 201,
+      data: offer,
+    })
+  } else {
+    res.status(404)
+    res.json('Offer not found')
+  }
 }
 
-function computeAvailableWeight(totalWeight: number, bookings: Booking[]): number{
-    const relevantBooking = bookings.filter(booking =>booking.status !== 'canceled')
-    const totalBookedWeight = relevantBooking.reduce((acc,booking)=>acc+booking.weight,0)
-    return totalWeight - totalBookedWeight
+function computeAvailableWeight(
+  totalWeight: number,
+  bookings: Booking[]
+): number {
+  const relevantBooking = bookings.filter(
+    (booking) => booking.status !== 'canceled'
+  )
+  const totalBookedWeight = relevantBooking.reduce(
+    (acc, booking) => acc + booking.weight,
+    0
+  )
+  return totalWeight - totalBookedWeight
 }
 
-export async function getAllOffers(req: Request, res: Response): Promise<void>{
-    const allOffers = await _getAllOffers()
-    const enhancedOffers = allOffers?.map((offer: any)=>({
-        id : offer.id,
-        departureId : offer.departureId,
-        arrivalId : offer.arrivalId,
-        weight : offer.weight,
-        availableWeight : computeAvailableWeight(offer.weight, offer.bookings),
-        etd : offer.etd,
-        eta : offer.eta,
-        bookable : offer.bookable,
-        price : offer.price,
-    }))
-   res.status(201)
-   res.json({statusCode : 201,
-    data : enhancedOffers
-   })
+export async function getAllOffers(req: Request, res: Response): Promise<void> {
+  const query = req.query
+  const page = Number(query.page) || 0
+  const limit = Number(query.limit) || 10
+  const allOffers = await OfferRepository.getAllOffers(limit, page)
+  const enhancedOffers = allOffers?.map((offer: any) => ({
+    id: offer.id,
+    departureId: offer.departureId,
+    arrivalId: offer.arrivalId,
+    weight: offer.weight,
+    availableWeight: computeAvailableWeight(offer.weight, offer.bookings),
+    etd: offer.etd,
+    eta: offer.eta,
+    bookable: offer.bookable,
+    price: offer.price,
+  }))
+  res.status(201)
+  res.json({
+    statusCode: 201,
+    data: enhancedOffers,
+    page,
+    nextPage: req.path + `?page=${Number(page) + 1}`,
+  })
 }
 
-
-export async function addBooking(req: Request, res: Response): Promise<void>{
-    const {weight, status, offerId} = req.body
-    const adding = await _addBooking(weight, status, offerId)
-    if(!adding){
-        res.status(400).json({statuscode : 400, message : "Not enough space available or offer non-bookable"})
-        return
-    } else {
-        res.status(201).json({statusCode : 201, message : "Booking added"})
-    }
+export async function addBooking(req: Request, res: Response): Promise<void> {
+  const { weight, status, offerId } = req.body
+  const adding = await OfferRepository.addBooking(weight, status, offerId)
+  if (!adding) {
+    res.status(400).json({
+      statuscode: 400,
+      message: 'Not enough space available or offer non-bookable',
+    })
+    return
+  } else {
+    res.status(201).json({ statusCode: 201, message: 'Booking added' })
+  }
 }
 
-export async function getBooking(req: Request, res: Response): Promise<void>{
-    const {id} = req.params
-    const booking = await _getBooking(Number(id))
-    if (!!booking){
-        res.status(201)
-        res.json({
-            statusCode : 201,
-            data : booking
-        })
-    } else{
-        res.status(404)
-        res.json("Booking not found")
-    }
+export async function getBooking(req: Request, res: Response): Promise<void> {
+  const { id } = req.params
+  const booking = await OfferRepository.getBooking(Number(id))
+  if (!!booking) {
+    res.status(201)
+    res.json({
+      statusCode: 201,
+      data: booking,
+    })
+  } else {
+    res.status(404)
+    res.json('Booking not found')
+  }
 }
--- apps/server/Models/offerModel.ts
+++ apps/server/Models/offerModel.ts
@@ -1,93 +1,119 @@
-import { Booking, Offer, PrismaClient } from "@prisma/client"
-import e from "express"
+import { Booking, Offer, PrismaClient } from '@prisma/client'
+import e from 'express'
 const prisma = new PrismaClient()
 
-export async function _addOffer(departure: string, arrival: string, weight: number, etd: string, eta: string, price: number, notes: string): Promise<void>{
-    await prisma.offer.create({
-        data: {
-            departureId: departure,
-            arrivalId: arrival,
-            weight,
-            etd,
-            eta,
-            price,
-            availableWeight : weight,
-            notes
-        },
-    })
+export async function addOffer(
+  departure: string,
+  arrival: string,
+  weight: number,
+  etd: string,
+  eta: string,
+  price: number,
+  notes: string
+): Promise<void> {
+  await prisma.offer.create({
+    data: {
+      departureId: departure,
+      arrivalId: arrival,
+      weight,
+      etd,
+      eta,
+      price,
+      availableWeight: weight,
+      notes,
+    },
+  })
 }
 
-export async function _getOffer(id: number): Promise<Offer | undefined | null>{
-    return (await prisma.offer.findUnique({
-        where:{
-            id: id
-        },
-        include :{
-            bookings : true,
-            departure: true,
-            arrival: true
-        }
-    }))
+export async function getOffer(id: number): Promise<Offer | undefined | null> {
+  return await prisma.offer.findUnique({
+    where: {
+      id: id,
+    },
+    include: {
+      bookings: true,
+      departure: true,
+      arrival: true,
+    },
+  })
 }
 
-export async function _getAllOffers(): Promise<any>{
-    return (await prisma.offer.findMany({
-        include:{      
-            bookings: true
-        }
-    }))
+export async function getAllOffers(limit = 10, page = 0): Promise<any> {
+  return await prisma.offer.findMany({
+    include: {
+      bookings: true,
+    },
+    take: limit,
+    skip: page * limit,
+  })
 }
 
-export async function _addBooking(weight: number, status: string, offerId: number): Promise<any>{
-    const offerData = await prisma.offer.findUnique({ //Get availableWeight and bookable from the needed offer
-        where: {
-            id: offerId
-        },
-        select:{
-            availableWeight : true,
-            bookable : true
-        }
-    })
-    if(!!offerData){ //If it's not undefined or null
-        const {availableWeight, bookable} = offerData
-        if(!bookable || availableWeight - weight < 0){ //If offer is not bookable OR weight of the booking is > to available weight of the offer, can't add booking
-            return false
-        }
+export async function addBooking(
+  weight: number,
+  status: string,
+  offerId: number
+): Promise<any> {
+  const offerData = await prisma.offer.findUnique({
+    //Get availableWeight and bookable from the needed offer
+    where: {
+      id: offerId,
+    },
+    select: {
+      availableWeight: true,
+      bookable: true,
+    },
+  })
+  if (!!offerData) {
+    //If it's not undefined or null
+    const { availableWeight, bookable } = offerData
+    if (!bookable || availableWeight - weight < 0) {
+      //If offer is not bookable OR weight of the booking is > to available weight of the offer, can't add booking
+      return false
     }
-    await prisma.booking.create({
-        data : {
-            weight,
-            status,
-            offerId,
-        }
-    })
-    await prisma.offer.update({ //Update offer's availableWeight with the new weight from the booking
-        where: {
-            id: offerId
-        },
-        data: {
-            availableWeight:{
-                decrement: weight
-            }
-        }
-    })
+  }
+  await prisma.booking.create({
+    data: {
+      weight,
+      status,
+      offerId,
+    },
+  })
+  await prisma.offer.update({
+    //Update offer's availableWeight with the new weight from the booking
+    where: {
+      id: offerId,
+    },
+    data: {
+      availableWeight: {
+        decrement: weight,
+      },
+    },
+  })
 }
 
-export async function _getBooking(id: number): Promise<Booking | undefined | null>{
-    return (await prisma.booking.findUnique({
-        where:{
-            id: id
-        },
-        include :{
-            offer: true,
-        }
-
-    }))
+export async function getBooking(
+  id: number
+): Promise<Booking | undefined | null> {
+  return await prisma.booking.findUnique({
+    where: {
+      id: id,
+    },
+    include: {
+      offer: true,
+    },
+  })
 }
 
-
-function computeAvailableWeight(totalWeight: number, bookings: Booking[]): number{
-    const relevantBooking = bookings.filter(booking =>booking.status !== 'canceled')
-    const totalBookedWeight = relevantBooking.reduce((acc,booking)=>acc+booking.weight,0)
-    return totalWeight - totalBookedWeight
+function computeAvailableWeight(
+  totalWeight: number,
+  bookings: Booking[]
+): number {
+  const relevantBooking = bookings.filter(
+    (booking) => booking.status !== 'canceled'
+  )
+  const totalBookedWeight = relevantBooking.reduce(
+    (acc, booking) => acc + booking.weight,
+    0
+  )
+  return totalWeight - totalBookedWeight
 }
